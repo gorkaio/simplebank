795efdab886afd4e45b1fe0e4f54e497ad3839e5
Gorka López de Torre Querejazu <gorka@gorka.io>
2023-06-15 09:41:43 +0200

    File and env configuration management

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/gorkaio/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load configuration:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err!= nil {
 		log.Fatal("Cannot connect to db: ", err)
 	}
